36ecfaa7574abd0e54a77c858f455b877bcba821
史涛 <st.email@qq.com>
2019-03-14 22:19:52 +0800

    线程创建的三种方式

--- src/thread/CreateThread.java
+++ src/thread/CreateThread.java
@@ -1,5 +1,9 @@
 package thread;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * 创建线程的几种方式
  */
@@ -12,7 +16,7 @@ public class CreateThread {
 
         @Override
         public void run() {
-            System.out.println("线程启动...");
+            System.out.println("thread继承，线程启动...");
         }
     }
 
@@ -23,7 +27,7 @@ public class CreateThread {
 
         @Override
         public void run() {
-            System.out.println("线程启动1...");
+            System.out.println("runnable,线程启动1...");
         }
     }
 
@@ -31,7 +35,13 @@ public class CreateThread {
     /**
      * 通过 futureTask 创建线程，可以设定返回值
      */
-    public 
+    public static class CallAbleTask implements Callable<String> {
+
+        @Override
+        public String call() throws Exception {
+            return "支持回调返回的，线程启动2...";
+        }
+    }
 
     /**
      * 主进程
@@ -39,11 +49,31 @@ public class CreateThread {
      * @param args
      */
     public static void main(String[] args) {
+        //1
         MyThread myThread = new MyThread();
         myThread.run();
+        //2
         RunableTask runableTask = new RunableTask();
         runableTask.run();
         new Thread(runableTask).run();
-
+        //3
+        CallAbleTask callAbleTask = new CallAbleTask();
+        try {
+            String call = callAbleTask.call();
+            System.out.println(call);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //3-1 
+        FutureTask<String> futureTask = new FutureTask<>(new CallAbleTask());
+        new Thread(futureTask).start();
+        try {
+            String s = futureTask.get();
+            System.out.println(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
